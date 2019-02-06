76d8781043db2461e42c7aa7c6fe58247d7bd6a7
Carlos Cartagena <ccart79@gmail.com>
2019-02-05 18:36:59 -0600

    Commit Added Category remove feature

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -52,6 +52,34 @@ namespace Ecommerce.Controllers
 
             return View(addCategoryViewModel);
         }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Category";
+            ViewBag.categories = context.Categories.ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Remove(int[] categoryIds)
+        {
+            foreach (int categoryId in categoryIds)
+            {
+                ProductCategory theCategory = context.Categories.Single(c => c.ID == categoryId);
+                context.Categories.Remove(theCategory);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/");
+
+        }
+
+
     }
+
+
+
 }
 
